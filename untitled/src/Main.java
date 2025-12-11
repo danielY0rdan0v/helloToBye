55c34896e2c9604def3c63300452da94a78e68bf
Daniel <daniburgas2003@gmail.com>
2025-12-12 01:23:27 +0200

    create Main

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
 
-        System.out.println("Test");
+        System.out.println("Now I can continue make changes to my project :)");
     }
 }
