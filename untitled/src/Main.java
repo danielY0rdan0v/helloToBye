d3bad397b87fad68a0e581f32645f13a115ab073
Daniel <daniburgas2003@gmail.com>
2025-12-11 21:16:47 +0200

    create Main

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
 
-        System.out.println("I am eating");
+        System.out.println("I am sleeping");
     }
 }
